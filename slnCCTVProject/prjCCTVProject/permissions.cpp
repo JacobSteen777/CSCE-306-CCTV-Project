4a0a72454b750f6a9ec13d624a872ddbdb46acec
JacobSteen777 <116609830+JacobSteen777@users.noreply.github.com>
2022-12-11 18:15:24 -0600

    implements permission

---
+++ slnCCTVProject/prjCCTVProject/permissions.cpp
@@ -0,0 +1,24 @@
+#include <string>
+
+class Permissions {
+private:
+	std::string permission;
+
+public:
+	// Constructor to initialize the permission
+	Permissions(std::string permission) {
+		this->permission = permission;
+	}
+
+	// Method to get the permission
+	std::string getPermission() {
+		return permission;
+	}
+
+	// Method to check the permission
+	bool checkPermission() {
+		// Check if the permission is valid and return the result
+		return (permission == "read" || permission == "write" || permission == "execute");
+	}
+
+};
